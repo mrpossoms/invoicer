fc76d85174c36c15b2239f71f3b277d2af7941c4
Kirk Roerig <mr.possoms@gmail.com>
2019-01-12 11:04:41 -0700

    Scrapes and concatenates git commit messages

--- commits.py
+++ commits.py
@@ -4,63 +4,62 @@ import workweek
 import re
 from subprocess import Popen, PIPE, STDOUT
 
+
 class Commit:
-	def __init__(self, repo, keys, msg):
-		self.repo = repo
-		self.message = msg
+    def __init__(self, repo, keys, msg):
+        self.repo = repo
+        self.message = msg
 
-		self.author = keys['Author']
-		_, month, day, time, year, tmz = keys['Date'].split(' ')
+        self.author = keys['Author']
+        _, month, day, time, year, tmz = keys['Date'].split(' ')
 
-		months = {
-			'Jan': 1,
-			'Feb': 2,
-			'Mar': 3,
-			'Apr': 4,
-			'May': 5,
-			'June': 6,
-			'July': 7,
-			'Aug': 8,
-			'Sept': 9,
-			'Oct': 10,
-			'Nov': 11,
-			'Dec': 12 
-		}
-		year = int(year)
-		month = months[month]
-		day = int(day)
-		self.date = datetime.datetime(year, month, day)
-        
-        def __str__(self):
-            return 'Date: {}\nAuthor: {}\nMessage: {}\n'.format(str(self.date), self.author, self.message)
+        months = {
+            'Jan': 1,
+            'Feb': 2,
+            'Mar': 3,
+            'Apr': 4,
+            'May': 5,
+            'June': 6,
+            'July': 7,
+            'Aug': 8,
+            'Sept': 9,
+            'Oct': 10,
+            'Nov': 11,
+            'Dec': 12
+        }
+        year = int(year)
+        month = months[month]
+        day = int(day)
+        self.date = datetime.datetime(year, month, day)
+
+    def __str__(self):
+        return 'Date: {}\nAuthor: {}\nMessage: {}\n'.format(str(self.date), self.author, self.message)
+
 
 def commits(repo_path, start, stop):
-	os.chdir(repo_path)
-	commits = []
+    os.chdir(repo_path)
+    cmd = 'git log --after={}-{}-{} --before={}-{}-{}'.format(start.year, start.month, start.day, stop.year, stop.month, stop.day + 1)
+    p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+    commit_texts = re.split('commit ([0-9]|[a-z])*\n', str(p.stdout.read(), 'utf-8'))
 
-	cmd = 'git log --after={}-{}-{} --before={}-{}-{}'.format(start.year, start.month, start.day, stop.year, stop.month, stop.day + 1)
-	p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+    def key_value(line):
+        parts = line.split(': ')
+        return parts[0], parts[1]
 
-	commit_texts = re.split('commit ([0-9]|[a-z])*\n', p.stdout.read())
+    commits = []
+    for commit_text in commit_texts:
+        parts = commit_text.split('\n\n    ')
+        if len(parts) < 2: continue
 
-	def key_value(line):
-		parts = line.split(': ')
-		if len(parts) != 2: return None, None
-		return parts[0], parts[1]
-	
-	for commit_text in commit_texts:
-		parts = commit_text.split('\n\n    ')
-		if len(parts) < 2: continue	
+        key_lines = parts[0]
+        msg = parts[1].lstrip().rstrip()
+        kvps = {}
 
-		key_lines = parts[0]
-		msg       = parts[1].lstrip().rstrip()
-		kvps      = {}
+        for key_line in key_lines.split('\n'):
+            key, value = key_value(key_line)
+            if key is not None:
+                kvps[key] = value.lstrip().rstrip()
 
-		for key_line in key_lines.split('\n'):
-			key, value = key_value(key_line)
-			if key != None: kvps[key] = value.lstrip().rstrip()
+        commits.append(Commit(repo_path, kvps, msg))
 
-		print('----')
-
-		c = Commit(repo_path, kvps, msg)
-                print(c)
+    return commits
--- invoicer.py
+++ invoicer.py
@@ -3,6 +3,8 @@
 from flask import *
 import datetime
 from workweek import workweek
+from commits import commits
+
 
 app = Flask(__name__)
 
@@ -13,7 +15,7 @@ def date_str(dt):
 
 def total_time(work_days):
     total = 0
-    for _, hours in work_days:
+    for _, hours, _ in work_days:
         total += hours
 
     return total
@@ -32,24 +34,51 @@ def week_start_end(start):
 
     return start, end
 
+
+def get_commits(start, end, repo_paths):
+    commit_table = {}
+
+    for repo_path in repo_paths:
+        if len(repo_path) == 0: continue
+
+        for commit in commits(repo_path.rstrip(), start, end):
+            if commit.date in commit_table:
+                commit_table[commit.date] += [commit]
+            else:
+                commit_table[commit.date] = [commit]
+
+    return commit_table
+
+
 @app.route('/')
 def home():
 	now = datetime.datetime.now()
 	return render_template('invoicer.html', start_date="{}-{:02d}-{:02d}".format(now.year, now.month, now.day))
+
 
 @app.route('/week/')
 def week():
     week_date = request.args.get('date')
     author    = request.args.get('author')
     repos_str = request.args.get('repos')
-    
+    repos = repos_str.split('\n')
+
     start, end = week_start_end(week_date)
 
     work = workweek(start, end)
+    commits = get_commits(start, end, repos)
     days = []
 
     for key in work:
-        days += [(date_str(key), round(work[key], 1))]
+        commit_messages = ''
+
+        if key not in commits: continue
+
+        for commit in commits[key]:
+            if author in commit.author:
+                commit_messages += commit.message + '. '
+
+        days += [(date_str(key), round(work[key], 1), commit_messages)]
 
     now = datetime.datetime.now()
     week = { 
@@ -57,6 +86,7 @@ def week():
             'week_end': date_str(end),
             'now': date_str(datetime.datetime.now()),
             'total': total_time(days)
+
             }
 
     return render_template('invoice.html', week=week)
